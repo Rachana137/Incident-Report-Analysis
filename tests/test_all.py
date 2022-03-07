36f848d413b15414682f56b9527743622b50e030
Rachana137 <v.rachanachoudary@gmail.com>
2022-03-07 19:27:08 +0000

    updated on 3/7/2022

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,19 +1,15 @@
 import pytest
 from project0 import project0 as p
 import sqlite3
-
 url='https://www.normanok.gov/sites/default/files/documents/2022-02/2022-02-01_daily_incident_summary.pdf'
 dbase='normanpd.db'
-
 def test_fetchincidents():
     data=p.fetchincidents(url)
     assert type(data)==bytes
-
 def test_extractincidents():
     data=p.fetchincidents(url)
     incidents=p.extractincidents(data)
     assert type(incidents)==list
-
 def test_extractincidents1():
     data=p.fetchincidents(url)
     incidents=p.extractincidents(data)
@@ -24,11 +20,19 @@ def test_extractincidents1():
         else:
             A=A.append(incidents[i])
     assert len(A)==0
-
+def test_extractincidents1():
+    data=p.fetchincidents(url)
+    incidents=p.extractincidents(data)
+    A=[]
+    for i in incidents:
+        if len(i)==5:
+            continue
+        else:
+            A=A.append(incidents[i])
+    assert len(A)==0
 def test_createdb():
     db=p.createdb()
     assert db=='normanpd.db'
-
 def test_populatedb():
     db = p.createdb()
     data=p.fetchincidents(url)
@@ -39,7 +43,6 @@ def test_populatedb():
     cur.execute('''select * from incidents''')
     assert cur.fetchall() is not None
     sql.close()
-
 def test_status():
     db = p.createdb()
     data=p.fetchincidents(url)
@@ -50,5 +53,5 @@ def test_status():
     cur = sql.cursor()
     cur.execute('''select nature,count(nature) as cnt from incidents group by nature order by cnt DESC''')
     assert stat is not None
-
+    
 
